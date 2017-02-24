938633a98b82f8484ac91fb863e6027cb015fba7
GreatDrummer2 <phillippochta@yahoo.com>
2017-02-24 10:02:52 +0200

    ready part 1

--- 1.js
+++ 1.js
@@ -1,9 +1,8 @@
 (function($){
  $(function(){
-  $('tabs_cap').on('click', 'li:not(.activated)', function() {
-    $(this)
-      .addClass('activated').siblings().removeClass('activated')
-      .closest('div.tabs').find('div.content').removeClass('active').eq($(this).index()).addClass('active');
+  $("div.tabs > ul.tabs_cap > li").on('click',function(){
+    $(this).addClass('activated').siblings().removeClass('activated')
+    .closest('div.tabs').find('div.content').removeClass('active').eq($('li').index($(this))).addClass('active');
   });
  })
 })(jQuery);
